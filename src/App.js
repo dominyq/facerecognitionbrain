8623fac9a6ddd77467c908c002bd28eaed1182e3
Dominyka <dominyka.taseva@yahoo.com>
2019-09-04 08:18:26 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation.js';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
 
-     {/* <Logo />
+       <Logo />
       <ImageLinkForm />
-     <faceRecognition /> */ }
+             {/*<faceRecognition /> */ }
    </div>
   )
 }
