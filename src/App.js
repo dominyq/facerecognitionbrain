87f7973849a1aae267f5123a0f14ed4831498005
Dominyka <dominyka.taseva@yahoo.com>
2019-09-06 21:00:36 +0300

    2update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import './App.css';
-import Navigation from './components/Navigation/Navigation.js';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import React from "react";
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation.js";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import Particles from 'react-particles-js';
 
-function App() {
+const particlesOptions = {
+  particles: {
+   number: {
+     value: 100,
+     density: {
+       enable: true,
+       value_area: 700
+     }
+   }
+  }
+}
+
+class App extends React.Component {
+  constructor() {
+  super()
+  this.state = {
+    input: '',
+  }
+}
+
+onInputChange = (event) => {
+console.log(event.target.value);
+}
+
+onButtonSubmit = () => {
+  console.log('click');
+}
+
+render() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params={particlesOptions} 
+        />
       <Navigation />
-
-       <Logo />
-      <ImageLinkForm />
-             {/*<faceRecognition /> */ }
-   </div>
-  )
+      <Logo />
+      <Rank />
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+      {/*<faceRecognition /> */}
+    </div>
+  );
+  }
 }
 
 export default App;
